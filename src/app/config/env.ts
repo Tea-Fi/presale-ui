d1ca3c969645cc7c2a65c065cee53eebf760be07
Alex Cryp <alex@syndika.co>
2024-07-03 15:48:55 +0300

    point to https api endpoint

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -2,4 +2,4 @@ export const WALLET_CONNECT_PROJECT_ID = import.meta.env.VITE_WALLET_CONNECT_PRO
 export const ALCHEMY_PROVIDER_KEY = import.meta.env.VITE_ALCHEMY_PROVIDER_KEY ?? "GG_D-CF5l3hAKKD9zzEpll4LSfe-RDXz";
 export const INFURA_URL = import.meta.env.VITE_PUBLIC_INFURA_URL ?? "https://eth-mainnet.public.blastapi.io";
 export const SUPPORTED_NETWORK = import.meta.env.VITE_PUBLIC_SUPPORTED_NETWORK ?? 1;
-export const API_URL = import.meta.env.VITE_API_URL ?? "http://presale-api.tea-fi.com";
+export const API_URL = import.meta.env.VITE_API_URL ?? "https://presale-api.tea-fi.com";
