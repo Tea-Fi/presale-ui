c06327fb894385d01beb3bd15c47bf657b7ffd52
Leahu Ion <ionleahu96@gmail.com>
2024-07-09 19:44:49 +0300

    fix switch network

--- src/app/config/wagmi-config.ts
+++ src/app/config/wagmi-config.ts
@@ -1,35 +1,28 @@
-import { getDefaultConfig } from 'connectkit';
-import { mainnet, sepolia } from 'viem/chains';
-import { createConfig, http } from 'wagmi';
-import { ALCHEMY_PROVIDER_KEY, WALLET_CONNECT_PROJECT_ID } from './env';
-
+import { getDefaultConfig } from "connectkit";
+import { mainnet, sepolia } from "viem/chains";
+import { createConfig, http } from "wagmi";
+import { ALCHEMY_PROVIDER_KEY, WALLET_CONNECT_PROJECT_ID } from "./env";
 
 export const wagmiConfig = createConfig(
-	getDefaultConfig({
-		// Your dApps chains
-		chains: [
-			sepolia,
-			mainnet
-		],
-		
-		transports: {
-			// RPC URL for each chain
-			[mainnet.id]: http(
-				`https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_PROVIDER_KEY}`
-			),
-			[sepolia.id]: http(
-				`https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_PROVIDER_KEY}`
-			),
-		},
+  getDefaultConfig({
+    // Your dApps chains
+    chains: [mainnet],
 
-		// Required API Keys
-		walletConnectProjectId: WALLET_CONNECT_PROJECT_ID,
+    transports: {
+      // RPC URL for each chain
+      [mainnet.id]: http(
+        `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_PROVIDER_KEY}`
+      ),
+    },
 
-		// Required App Info
-		appName: 'Teaswap',
-		// Optional App Info
-		appDescription: 'Teaswap presale',
-		appUrl: 'https://presale.tea-fi.com',
-		appIcon: 'https://presale.tea-fi.com/favicon.svg'
-	})
+    // Required API Keys
+    walletConnectProjectId: WALLET_CONNECT_PROJECT_ID,
+
+    // Required App Info
+    appName: "Teaswap",
+    // Optional App Info
+    appDescription: "Teaswap presale",
+    appUrl: "https://presale.tea-fi.com",
+    appIcon: "https://presale.tea-fi.com/favicon.svg",
+  })
 );
