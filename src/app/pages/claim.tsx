77761243a5e4949964468d106f9dc270cd301926
Alex Cryp <alex@syndika.co>
2024-07-12 09:56:54 +0300

    adjust claim page

--- src/app/pages/claim.tsx
+++ src/app/pages/claim.tsx
@@ -16,7 +16,7 @@ export const Claim = () => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [, setIsError] = useState<boolean>(false);
   const [totalSoldTea, setTotalSoldTea] = useState<string | number>(0);
-  const [totalAvailableTea, setTotalAvailableTea] = useState<string | number>(0);
+  // const [totalAvailableTea, setTotalAvailableTea] = useState<string | number>(0);
 
   const chainId = getChainId(wagmiConfig);
   const account = getAccount(wagmiConfig);
@@ -46,10 +46,10 @@ export const Claim = () => {
         totalAvailableTeaResult.isSuccess
       ) {
         const totalSoldNormal = parseHumanReadable(totalSoldTeaResult.data as bigint, 18, 2);
-        const totalAvailableNormal = parseHumanReadable(totalAvailableTeaResult.data as bigint, 18, 0);
+        // const totalAvailableNormal = parseHumanReadable(totalAvailableTeaResult.data as bigint, 18, 0);
 
         setTotalSoldTea(totalSoldNormal.toLocaleString('en-US'));
-        setTotalAvailableTea(totalAvailableNormal.toLocaleString('en-US'));
+        // setTotalAvailableTea(totalAvailableNormal.toLocaleString('en-US'));
         
         setIsLoading(false);
         setIsError(false);
@@ -79,44 +79,37 @@ export const Claim = () => {
     }))
   );
 
+  const handleOptionsInfo = async () => {
+    const optionInfos = await Promise.all(tgeInfos.map((info: any) => 
+      readContract(wagmiConfig, {
+        abi: PRESALE_ABI,
+        address: PRESALE_CONTRACT_ADDRESS[chainId] as Address,
+        functionName: 'saleOptions',
+        args: [info.optionId],
+      })
+    ));
+
+    const balances = [];
+
+    for(const info of optionInfos) {
+      const balance = await readContract(wagmiConfig, {
+        abi: erc20Abi,
+        // [3] means presaleToken in contract struct
+        address: info[3] as Address,
+        functionName: 'balanceOf',
+        args: [account.address as Address],
+      });
+
+      balances.push(balance);
+    }
+
+    return [balances, optionInfos];
+  };
+
   useEffect(() => {
-    const handleOptionsInfo = async () => {
-      try {
-        const optionInfos = await Promise.all(tgeInfos.map((info: any) => 
-          readContract(wagmiConfig, {
-            abi: PRESALE_ABI,
-            address: PRESALE_CONTRACT_ADDRESS[chainId] as Address,
-            functionName: 'saleOptions',
-            args: [info.optionId],
-          })
-        ));
-
-
-        const balances = [];
-
-        for(const info of optionInfos) {
-          const balance = await readContract(wagmiConfig, {
-            abi: erc20Abi,
-            // [3] means presaleToken in contract struct
-            address: info[3] as Address,
-            functionName: 'balanceOf',
-            args: [account.address as Address],
-          });
-
-          balances.push(balance);
-        }
-
-        return [balances, optionInfos];
-      } catch (e) {
-        console.error(e);
-      }
-    };
-
     handleOptionsInfo().then((data: any) => {
       const balances = data[0];
       const optionInfos = data[1];
-
-      // console.log(data[1][0][0])
       
       if(balances === undefined) {
         return;
@@ -133,7 +126,7 @@ export const Claim = () => {
       }
       setTgeInfos(newInfo);
     });
-  }, [account]);
+  }, []); // <-- if add here account than you get infinite requests to RPC...
 
   const handleCalculateTgeAmount = (balance?: number, tge?: bigint) => {
     if(balance !== undefined && tge !== undefined) {
@@ -148,16 +141,20 @@ export const Claim = () => {
       <h1>Claim</h1>
       <p>Token claim will be available at $TEA token's TGE</p>
 
-      <span className='text-center'>Total TEA {isLoading ? <Spinner /> : `${totalSoldTea}/${totalAvailableTea}`}</span>
+      <span className='text-center'>Total Bought: {isLoading ? <Spinner /> : `${totalSoldTea}`} $TEA</span>
 
       <div className="mt-10 inline-flex gap-4 px-8 justify-center w-full max-w-[1400px] items-center mx-auto flex-wrap">
         {tgeInfos && tgeInfos.length ? 
           tgeInfos.map((projectInfo: any, index: number) => 
             <Card key={index} className='size-64'>
-              <CardTitle>You bought:</CardTitle>
+              <CardTitle>{projectInfo.price} / $TEA</CardTitle>
               <CardDescription className='flex flex-col gap-3'>
-                <span className='text-lg'>{projectInfo.balance !== null ? projectInfo.balance.toLocaleString('en-US') : <Spinner />} TEA by {projectInfo.price}</span>
-                <span className='mt-2 text-base'>You will receive at TGE: {projectInfo.tge !== null && handleCalculateTgeAmount(projectInfo.balance, projectInfo.tge) !== null ? ` ${handleCalculateTgeAmount(projectInfo.balance, projectInfo.tge)?.toLocaleString()} - ${projectInfo.tge}%` : <Spinner />}</span>
+                <span className='text-lg'>{projectInfo.balance !== null ? projectInfo.balance.toLocaleString('en-US') : <Spinner />} $TEA</span>
+                <span className='mt-2 text-base'>
+                  Claim at TGE ({`${projectInfo.tge}%`}): {projectInfo.tge !== null && handleCalculateTgeAmount(projectInfo.balance, projectInfo.tge) !== null
+                    ? ` ${handleCalculateTgeAmount(projectInfo.balance, projectInfo.tge)?.toLocaleString()} $TEA`
+                    : <Spinner />}
+                </span>
               </CardDescription>
             </Card>
           )
