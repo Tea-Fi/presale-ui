0cf988fc601ef4759338a82e7bc83d12cd0a2c63
Nice Arti <nice_arti@mail.ru>
2024-07-12 18:06:01 +0300

    feat: start adding fix for usdt | change contdown to 30 sept

--- src/app/components/countdown-sm.tsx
+++ src/app/components/countdown-sm.tsx
@@ -85,8 +85,6 @@ export const CountdownSmall = ({ finishTime }: {finishTime?: Date | string | num
                     </div>
                   </CircularProgressbarWithChildren>
                 </div>
-  
-                  
               </div>
             </div>
           );
