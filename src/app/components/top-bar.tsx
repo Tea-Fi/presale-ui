1863aea27aeca2d6353b1af009e670e89539625a
Nice Arti <nice_arti@mail.ru>
2024-07-12 15:16:36 +0300

    feat: add timer in top of header

--- src/app/components/top-bar.tsx
+++ src/app/components/top-bar.tsx
@@ -11,8 +11,9 @@ import { wagmiConfig, ChainId } from "../config";
 import { RiMenu3Fill } from "react-icons/ri";
 import { Button } from "./ui";
 import { useModal } from "connectkit";
-
-
+import Countdown from 'react-countdown';
+import { buildStyles, CircularProgressbar, CircularProgressbarWithChildren } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 export const TopBar = ({
   isBuyPageActive,
   isClaimPageActive,
@@ -43,12 +44,113 @@ export const TopBar = ({
     },
   });
 
+  
+  const TimerCompletion = () => <span className="text-white">Presale is ended!</span>;
+
+  // Renderer callback with condition
+  const renderer = ({ days, hours, minutes, seconds, completed}:{ days: number, hours: number, minutes: number, seconds: number, completed: boolean }) => {
+    if (completed) {
+      // Render a completed state
+      return <TimerCompletion />;
+    } else {
+      // Render a countdown
+      const d = days;
+      const h = hours < 10 ? `0${hours}`: hours;
+      const m = minutes < 10 ? `0${minutes}`: minutes;
+      const s = seconds < 10 ? `0${seconds}`: seconds;
+      return <div className="flex flex-col text-white">
+          <div className="inline-flex h-10 gap-1 ">
+            <div className="size-10">
+              <CircularProgressbarWithChildren
+                value={d}
+                maxValue={50}
+                minValue={0}
+                styles={buildStyles({
+                    pathColor: `#ff00a4`,
+                    textColor: `#ff00a4`,
+                })}
+              >
+                <div className="relative flex flex-col gap-0 text-[#ff00a4] text-center mb-2">
+                  <span className="text-[14px] leading-0 h-fit">{d}</span>
+                  <span className="absolute text-[10px] top-[15px] left-1/2 -translate-x-1/2">D</span>
+                </div>
+              </CircularProgressbarWithChildren>
+            </div>
+            <div className="size-10">
+              <CircularProgressbarWithChildren
+                  value={+h}
+                  maxValue={23}
+                  minValue={0}
+                  styles={buildStyles({
+                      pathColor: `#ff00a4`,
+                      textColor: `#ff00a4`,
+                  })}
+              >
+                <div className="relative flex flex-col gap-0 text-[#ff00a4] text-center mb-2">
+                  <span className="text-[14px] leading-0 h-fit">{h}</span>
+                  <span className="absolute text-[10px] top-[15px] left-1/2 -translate-x-1/2">H</span>
+                </div>
+              </CircularProgressbarWithChildren>
+            </div>
+            <div className="size-10">
+            <CircularProgressbarWithChildren
+                  value={+m}
+                  maxValue={59}
+                  minValue={0}
+                  styles={buildStyles({
+                      pathColor: `#ff00a4`,
+                      textColor: `#ff00a4`,
+                  })}
+              >
+                <div className="relative flex flex-col gap-0 text-[#ff00a4] text-center mb-2">
+                  <span className="text-[14px] leading-0 h-fit">{m}</span>
+                  <span className="absolute text-[10px] top-[15px] left-1/2 -translate-x-1/2">M</span>
+                </div>
+              </CircularProgressbarWithChildren>
+            </div>
+            <div className="size-10">
+              <CircularProgressbarWithChildren
+                  value={+s}
+                  maxValue={59}
+                  minValue={0}
+                  styles={buildStyles({
+                      pathColor: `#ff00a4`,
+                      textColor: `#ff00a4`,
+                  })}
+              >
+                <div className="relative flex flex-col gap-0 text-[#ff00a4] text-center mb-2">
+                  <span className="text-[14px] leading-0 h-fit">{s}</span>
+                  <span className="absolute text-[10px] top-[15px] left-1/2 -translate-x-1/2">S</span>
+                </div>
+              </CircularProgressbarWithChildren>
+            </div>
+
+              
+          </div>
+          {/* <span className="text-center bg-red-200">Presale countdown timer</span>
+          <span className="text-white text-center">{d} {d === 1 ? `day` : 'days'} {h}:{m}:{s}</span> */}
+        </div>;
+    }
+  };
+
+  const finishTime = new Date('08/31/2024 23:59:59');
+  console.log(finishTime.getTime())
 
   return (
     <div className="w-full max-h-24 inline-flex justify-between items-center px-5 py-3">
-      <span className="md:w-[228px]">
+      <div className="inline-flex items-center gap-5 md:w-[250px]">
         <TeaSwapLogoAsset className="size-10"/>
-      </span>
+
+        
+        <Countdown
+          date={finishTime}
+          intervalDelay={0}
+          precision={3}
+          renderer={renderer}
+        />
+
+      </div>
+
 
       <div className="inline-flex items-center gap-2 min-w-[100px] h-16 w-fit bg-black text-white rounded-full p-3 border dark:border-white/[0.2]">
         <NavLink 
