2552cd291db6bada49f63035cd2022985937ced2
Constantin Amurov <constantin.amurov@assist.ro>
2024-08-15 14:32:06 +0300

    feat(ttt-276): Add Presale Headline

---
+++ src/app/components/presale-headline/presale-headline.tsx
@@ -0,0 +1,7 @@
+
+export const PresaleHeadline = ()=> {
+
+    return <div className="text-center text-white m-2 mb-10">
+       <h1 className="text-7xl lg:text-9xl typed-text">Join the Presale</h1>
+    </div>
+}
