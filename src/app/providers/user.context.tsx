493ac8379e4d05984e6b9c5cc7db5ecf66188e15
Constantin Amurov <constantin.amurov@assist.ro>
2024-08-16 16:58:18 +0300

    feat(ttt-275): Add change Ambassador Code through url + login using referal code from queryparam

--- src/app/providers/user.context.tsx
+++ src/app/providers/user.context.tsx
@@ -1,107 +1,112 @@
-import type { FunctionComponent, ReactNode } from 'react';
-import { createContext, useContext, useState, useCallback, useEffect } from 'react';
-import { getReferralTreeByCode } from '../utils/referrals';
+import type {FunctionComponent, ReactNode} from 'react';
+import {createContext, useContext, useState, useCallback, useEffect} from 'react';
+import {getReferralTreeByCode} from '../utils/referrals';
+import {useLocalStorage} from "usehooks-ts";
+import {useReferralStore} from "../state/referal.store.ts";
 
 export interface LoginResponse {
-  status: LoginStatus | null;
+    status: LoginStatus | null;
 }
 
 export enum LoginStatus {
-  LOGGED_OUT = 'not-logged-in',
-  WRONG_CODE = 'wrong-code',
-  CODE_VERIFIED = 'code-verified',
-  LOGGED_IN = 'logged-in',
+    LOGGED_OUT = 'not-logged-in',
+    WRONG_CODE = 'wrong-code',
+    CODE_VERIFIED = 'code-verified',
+    LOGGED_IN = 'logged-in',
 }
 
 interface TermsOfService {
-  agree: () => void;
-  disagree: () => void;
+    agree: () => void;
+    disagree: () => void;
 }
 
 export interface UserContext {
-  status: LoginStatus | null;
-  login: (code: string) => Promise<void>;
-  logout: () => void;
-  terms: TermsOfService; 
+    status: LoginStatus | null;
+    login: (code: string) => Promise<void>;
+    logout: () => void;
+    terms: TermsOfService;
 }
 
 export const UserContext = createContext<UserContext | null>(null);
 
-export const UserContextProvider: FunctionComponent<{ children: ReactNode }> = ({ children }) => {
-  const [status, setStatus] = useState<LoginStatus | null>(null);
+export const UserContextProvider: FunctionComponent<{ children: ReactNode }> = ({children}) => {
+    const [status, setStatus] = useState<LoginStatus | null>(null);
+    const {setReferralId,setReferralCode} = useReferralStore();
 
-  async function login(code: string): Promise<void> {
-    try {
-      const referralTree = await getReferralTreeByCode(code);
+    const [agreedToTerms, setAgreedToTerms] = useLocalStorage<boolean>('agreedToTerms', false);
 
-      if (referralTree == undefined) {
-        setStatus(LoginStatus.WRONG_CODE);
-        setTimeout(() => setStatus(LoginStatus.LOGGED_OUT), 3000);
-      } else {
-        window.localStorage.setItem('referral', String(referralTree.id));
-        window.localStorage.setItem('referral-code', String(referralTree.referral));
-        
-        if (window.localStorage.getItem('agreedToTerms') === 'true') {
-          setStatus(LoginStatus.LOGGED_IN);
+    async function login(code: string): Promise<void> {
+        try {
+            const referralTree = await getReferralTreeByCode(code);
+
+            if (!referralTree) {
+                setStatus(LoginStatus.WRONG_CODE);
+                setTimeout(() => setStatus(LoginStatus.LOGGED_OUT), 3000);
+                return;
+            }
+
+            setReferralId(referralTree.id);
+            setReferralCode(referralTree.referral || "");
+
+            if (!agreedToTerms) {
+                setStatus(LoginStatus.CODE_VERIFIED);
+                return;
+            }
+
+            setStatus(LoginStatus.LOGGED_IN);
+        } catch (error) {
+            setStatus(LoginStatus.WRONG_CODE);
+            setTimeout(() => setStatus(LoginStatus.LOGGED_OUT), 3000);
+        }
+    }
+
+    async function logout() {
+        setStatus(LoginStatus.LOGGED_OUT);
+        setReferralId(null);
+        setReferralCode(null);
+    }
+
+
+    const agreeToTerms = useCallback(async () => {
+        setAgreedToTerms(true);
+        setStatus(LoginStatus.LOGGED_IN);
+
+    }, []);
+
+    const disagreeToTerms = useCallback(async () => {
+        setAgreedToTerms(false)
+        setStatus(LoginStatus.LOGGED_OUT);
+    }, []);
+
+    useEffect(() => {
+
+        if (agreedToTerms) {
+            setStatus(LoginStatus.LOGGED_IN);
         } else {
-          setStatus(LoginStatus.CODE_VERIFIED);
+            setStatus(LoginStatus.LOGGED_OUT);
         }
-      }
-    } catch (error) {
-      setStatus(LoginStatus.WRONG_CODE);
-      setTimeout(() => setStatus(LoginStatus.LOGGED_OUT), 3000);
-    }
-  }
+    }, []);
 
-  async function logout() {
-    setStatus(LoginStatus.LOGGED_OUT);
-
-    window.localStorage.removeItem('referral');
-    window.localStorage.removeItem('referral-code');
-  }
-
-  const validateAgreedToTerms = useCallback(async (agreedToTerms: boolean) => {
-    if (agreedToTerms) {
-      setStatus(LoginStatus.LOGGED_IN);
-    } else {
-      setStatus(LoginStatus.LOGGED_OUT);
-    }
-  }, []);
-
-  const agreeToTerms = useCallback(async () => {
-    window.localStorage.setItem('agreedToTerms', 'true');
-    validateAgreedToTerms(true);
-  }, [validateAgreedToTerms]);
-
-  const disagreeToTerms = useCallback(async () => {
-    window.localStorage.setItem('agreedToTerms', 'false');
-    validateAgreedToTerms(false);
-  }, [validateAgreedToTerms]);
-
-  useEffect(() => {
-    validateAgreedToTerms(window.localStorage.getItem('agreedToTerms') === 'true');
-  }, [validateAgreedToTerms]);
-
-  return (
-    <UserContext.Provider
-      value={{
-        status,
-        login,
-        logout,
-        terms: {
-          agree: agreeToTerms,
-          disagree: disagreeToTerms
-        }
-      }}
-      children={children}
-    />
-  );
+    return (
+        <UserContext.Provider
+            value={{
+                status,
+                login,
+                logout,
+                terms: {
+                    agree: agreeToTerms,
+                    disagree: disagreeToTerms
+                }
+            }}
+            children={children}
+        />
+    );
 };
 
 export const useUserContext = (): UserContext => {
-  const contextValue = useContext(UserContext);
-  if (!contextValue) {
-    throw new Error('Tried to use template context from outside the provider');
-  }
-  return contextValue;
+    const contextValue = useContext(UserContext);
+    if (!contextValue) {
+        throw new Error('Tried to use template context from outside the provider');
+    }
+    return contextValue;
 };
