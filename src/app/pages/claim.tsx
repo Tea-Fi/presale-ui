4d470edd8c0cea69e435d59ac2b4a7324d2b6610
Leahu Ion <ion.leahu@syndika.co>
2024-10-16 16:11:41 +0300

    change texts

--- src/app/pages/claim.tsx
+++ src/app/pages/claim.tsx
@@ -3,30 +3,20 @@ import { useInvestmentInfos } from "../hooks/useInvestmentInfos";
 import { InvestmentInfo } from "../components/claim/investment-info";
 
 export const Claim = () => {
-  const {
-    totalSoldTeaPerAccount,
-    loading,
-    investmentInfos,
-    refetchInvestmentInfo,
-  } = useInvestmentInfos();
+  const { totalSoldTeaPerAccount, loading, investmentInfos, refetchInvestmentInfo } = useInvestmentInfos();
 
   return (
     <div className="claim flex flex-col mx-auto">
       <h1>Claim</h1>
-      <p>Token claim will be available at $TEA token's TGE</p>
+      <p>Token claim will be available after $TEA token's TGE</p>
 
-      <span className="text-center">
-        Total Bought: {loading ? <Spinner /> : `${totalSoldTeaPerAccount} $TEA`}
-      </span>
+      <span className="text-center">Total Bought: {loading ? <Spinner /> : `${totalSoldTeaPerAccount} $TEA`}</span>
 
       <div className="flex gap-4 mt-10 flex-wrap justify-center mb-4">
         {!loading &&
           investmentInfos.map((investmentInfo, index) => (
             <div key={index} className="flex flex-col gap-4 ">
-              <InvestmentInfo
-                investmentInfo={investmentInfo}
-                refetchInvestmentInfo={refetchInvestmentInfo}
-              />
+              <InvestmentInfo investmentInfo={investmentInfo} refetchInvestmentInfo={refetchInvestmentInfo} />
             </div>
           ))}
       </div>
