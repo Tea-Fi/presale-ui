749de2ec5dab83a92bad876b2eed4f907a58aa17
Leahu Ion <ion.leahu@syndika.co>
2024-11-01 14:36:00 +0200

    change UI

--- src/app/components/claim/vesting-card.tsx
+++ src/app/components/claim/vesting-card.tsx
@@ -36,22 +36,14 @@ export const VestingCard: React.FC<ClaimCardProps> = ({
   onClaimCallback,
 }) => {
   if (!vestingInfo || vestingInfo.tokensForVesting == 0n) return;
-  const {
-    claimed,
-    totalVested,
-    totalAmount,
-    totalInitialUnlock,
-    totalAmountBurn,
-  } = useSubgraphInfo(tokenAddress as Address);
+  const { claimed, totalVested, totalAmount, totalInitialUnlock, totalAmountBurn } = useSubgraphInfo(
+    tokenAddress as Address
+  );
 
   const dateEnd = formatDateToDDMMYY(vestingInfo.dateEnd);
 
   const parsedTotalVested = parseHumanReadable(totalVested, 18, 2);
-  const parsedTotalInitialUnlocked = parseHumanReadable(
-    totalInitialUnlock,
-    18,
-    2
-  );
+  const parsedTotalInitialUnlocked = parseHumanReadable(totalInitialUnlock, 18, 2);
   const parsedClaimed = parseHumanReadable(claimed, 18, 2);
   const parsedTotalAmountBurn = parseHumanReadable(totalAmountBurn, 18, 2);
   const parsedTotalAmount = parseHumanReadable(totalAmount, 18, 2);
@@ -71,28 +63,20 @@ export const VestingCard: React.FC<ClaimCardProps> = ({
         months={VESTING_PERIOD_MONTHS[vestingInfo.claimPercent]}
         dateStart={vestingInfo.dateStart}
       />
-      <ProgressBar
-        value1={parsedClaimed}
-        value2={parsedTotalVested}
-        maxValue={parsedTotalAmount}
-      />
+      <ProgressBar value1={parsedClaimed} value2={parsedTotalVested} maxValue={parsedTotalAmount} />
       <div className="flex justify-between text-sm">
+        <span className="text-[#f716a2]">You will get</span>
+        <span>{formatNumber(parsedTotalAmount)} $TEA</span>
+      </div>
+      <div className="flex justify-between text-sm mt-2">
         <span className="text-[#f716a2]">Vested</span>
-        <span>
-          {formatNumber(parsedTotalVested)}/{parsedTotalAmount} $TEA
-        </span>
+        <span>{formatNumber(parsedTotalVested)} $TEA</span>
       </div>
       <div className="flex justify-between text-sm mt-2 mb-5">
         <span className="text-[#f716a2]">Claimed</span>
-        <span>
-          {parsedClaimed}/{parsedTotalAmount} $TEA
-        </span>
+        <span>{formatNumber(parsedClaimed)} $TEA</span>
       </div>
-      <VestingButton
-        tokenAddress={tokenAddress!}
-        claimableValue={claimableValue}
-        onClaimCallback={onClaimCallback}
-      />
+      <VestingButton tokenAddress={tokenAddress!} claimableValue={claimableValue} onClaimCallback={onClaimCallback} />
     </Card>
   );
 };
